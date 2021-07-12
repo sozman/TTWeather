fd09fc5ee3d7686715d2643f6ef576e85496fa80
Sinan Özman <sinanozman@icloud.com>
2021-07-12 21:44:27 +0300

    Network Layer done

---
+++ TTWeather/Extension/Encodable+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Encodable+Extension.swift
+//  TTWeather
+//
+//  Created by Sinan Özman on 12.07.2021.
+//
+
+import Foundation
+
+extension Encodable {
+    /// Codable to dictionary for Alamofire Parameter
+    var dictionary: [String: Any]? {
+        guard let data = try? JSONEncoder().encode(self) else { return nil }
+        return (try? JSONSerialization.jsonObject(with: data, options: .allowFragments))
+            .flatMap {
+                $0 as? [String: Any]
+                
+            }
+    }
+}
